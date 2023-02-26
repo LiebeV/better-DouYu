6dd4d4b8353c055c24e61a706741932527cfa087
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-02-26 14:18:00 +0100

    Update and rename 优化斗鱼web播放器.user.js to 优化斗鱼and抖音web播放器.user.js
    add douyin live

--- 优化斗鱼and抖音web播放器.user.js
+++ 优化斗鱼and抖音web播放器.user.js
@@ -1,10 +1,11 @@
 // ==UserScript==
-// @name         优化斗鱼web播放器
+// @name         优化斗鱼/抖音web播放器
 // @namespace    https://www.liebev.site
-// @version      0.2.1
+// @version      0.3
 // @description  通过关闭直播间全屏时的背景虚化效果来解决闪屏的问题
 // @author       LiebeV
 // @match        https://www.douyu.com/*
+// @match        https://live.douyin.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=douyu.com
 // @grant        GM_addStyle
 // ==/UserScript==
@@ -19,8 +20,11 @@
         .watermark-442a18{
             display: none !important
         }
+        .xgplayer-dynamic-bg{
+            visibility: hidden !important
+        }
     `
     GM_addStyle(css)
 })();
-// 无法确认虚化背景类名是否不变，使用canvas代替类名定位，可能会关闭其他功能
-// 关闭角落处文字水印，图片水印无法通过css定位
+// 无法确认斗鱼虚化背景类名是否不变，暂时使用canvas代替类名定位，可能会关闭其他功能
+// 关闭角落处文字水印，图片水印来自后台推流端，无法通过css定位
