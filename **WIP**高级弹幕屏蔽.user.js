d5e39571b0bd1d716240c624c43267a9539dcde5
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-03-13 19:53:30 +0100

    Update **WIP**高级弹幕屏蔽.user.js

--- **WIP**高级弹幕屏蔽.user.js
+++ **WIP**高级弹幕屏蔽.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         高级弹幕屏蔽
 // @namespace    https://www.liebev.site
-// @version      0.1
+// @version      0.2
 // @description  douyu斗鱼，高级弹幕屏蔽，突破30级等级屏蔽限制
 // @author       LiebeV
 // @match        https://www.douyu.com/*
@@ -33,15 +33,25 @@
 const ulList = document.getElementById("js-barrage-list");
 console.log('已获取弹幕区');
 console.log(ulList);
-
 let bannedIds = GM_getValue('bannedIds', '[]');
+
 
 //比对id，更改父元素css
 async function ban(userId) {
+    console.log("ban函数接收到userId，进行放行判断");
+    console.log(userId);
     if (bannedIds.includes(userId)) {
-        const liElement = document.querySelector(`span[title="${userId}"]`).parentNode;
-        liElement.style.display = 'none';
-        console.log(`"${userId}" 的发言已被屏蔽!`);
+        console.log("弹幕进入屏蔽分支");
+        const spanElements = document.querySelectorAll(`span[title="${userId}"]`);
+        console.log("已选取需屏蔽的弹幕元素");
+        for (let i = spanElements.length - 1; i >= 0; i--) {
+            const spanElement = spanElements[i];
+            const liElement = spanElement.parentNode.parentNode;
+            liElement.style.display = 'none';
+            console.log(`"${userId}" 的发言已被屏蔽!`);
+        }
+    }else{
+        console.log("放行");
     }
 }
 
@@ -49,6 +59,7 @@ async function ban(userId) {
 async function getId(node) {
     const userId = node.querySelector("span.Barrage-nickName[title]").getAttribute("title");
     console.log('用户id：' + userId);
+    console.log("已将id传入ban函数");
     ban(userId);
 }
 
@@ -72,6 +83,7 @@ async function MO() {
 
 //通过setValue像存储器添加id
 async function addId(userIdInput) {
+    console.log(`addId接收到传入`);
     if (!bannedIds.includes(userIdInput)) {
         bannedIds.push(userIdInput);
         console.log(`已添加屏蔽用户： ${userIdInput}`);
@@ -81,15 +93,20 @@ async function addId(userIdInput) {
     }
 }
 
-GM_registerMenuCommand('添加屏蔽用户', showSettings);
-
 async function showSettings() {
     const userIdInput = prompt(`请输入要屏蔽发言的用户id：（当你下次进入房间时生效）`);
     if (userIdInput) {
+        console.log('接收到用户id，传入addId');
         addId(userIdInput);
     }
 }
 
+GM_registerMenuCommand('添加屏蔽用户', showSettings);
+
 (function () {
     MO();
 })();
+
+//v0.1 -- 依托答辩
+//v0.2 -- 右侧弹幕区屏蔽功能完善
+//（v0.3） -- 飘屏区弹幕屏蔽
