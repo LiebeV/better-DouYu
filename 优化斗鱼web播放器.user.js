dd1a2a3b3f5920cb3237ba3f9a5c78f43d93d471
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-03-13 23:36:30 +0100

    Update 优化斗鱼web播放器.user.js

--- 优化斗鱼web播放器.user.js
+++ 优化斗鱼web播放器.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         优化斗鱼web播放器
 // @namespace    https://www.liebev.site
-// @version      1.1.1
+// @version      1.2
 // @description  douyu优化斗鱼web播放器，通过关闭直播间全屏时的背景虚化效果来解决闪屏的问题，屏蔽独立直播间的弹幕显示，移除文字水印
 // @author       LiebeV
 // @license      MIT; https://opensource.org/licenses/MIT; LiebeV
