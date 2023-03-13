532969955058ece3767cd1b5fc7a32aaccfc2c23
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-03-13 23:34:24 +0100

    Update 优化斗鱼web播放器.user.js

--- 优化斗鱼web播放器.user.js
+++ 优化斗鱼web播放器.user.js
@@ -1,8 +1,8 @@
 // ==UserScript==
 // @name         优化斗鱼web播放器
 // @namespace    https://www.liebev.site
-// @version      1.1
-// @description  优化斗鱼web播放器，通过关闭直播间全屏时的背景虚化效果来解决闪屏的问题，屏蔽独立直播间的弹幕显示，移除文字水印
+// @version      1.1.1
+// @description  douyu优化斗鱼web播放器，通过关闭直播间全屏时的背景虚化效果来解决闪屏的问题，屏蔽独立直播间的弹幕显示，移除文字水印
 // @author       LiebeV
 // @license      MIT; https://opensource.org/licenses/MIT; LiebeV
 // @match        https://www.douyu.com/*
@@ -14,7 +14,7 @@
 
 'use strict';
 
-let roomIds = GM_getValue('roomIds', '[]');
+let roomIds = GM_getValue('roomIds', []);
 const userRoomIds = roomIds;
 
 async function blur() {
