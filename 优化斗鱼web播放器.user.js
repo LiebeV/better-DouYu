2df8faf8e99d4d35e44ffc0c1d96c95145de5fff
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-03-15 11:13:00 +0100

    现在添加（移除）房间号时不需要刷新网页就可以生效了

--- 优化斗鱼web播放器.user.js
+++ 优化斗鱼web播放器.user.js
@@ -1,10 +1,10 @@
 // ==UserScript==
 // @name         优化斗鱼web播放器
 // @namespace    https://www.liebev.site
-// @version      1.2
+// @version      1.3
 // @description  douyu优化斗鱼web播放器，通过关闭直播间全屏时的背景虚化效果来解决闪屏的问题，屏蔽独立直播间的弹幕显示，移除文字水印
 // @author       LiebeV
-// @license      MIT; https://opensource.org/licenses/MIT; LiebeV
+// @license      MIT; LiebeV
 // @match        https://www.douyu.com/*
 // @icon         https://www.google.com/s2/favicons?sz=64&domain=douyu.com
 // @grant        GM_registerMenuCommand
@@ -13,6 +13,10 @@
 // ==/UserScript==
 
 'use strict';
+
+//更新日志，v1.3，现在添加（移除）房间号时不需要刷新网页就可以生效了
+//已知问题，无
+//更新计划，无（请关注主页新项目--“全等级弹幕屏蔽”）
 
 let roomIds = GM_getValue('roomIds', []);
 const userRoomIds = roomIds;
@@ -51,16 +55,19 @@ async function updateRoomId(roomId) {
         roomIds.push(roomId);
         console.log(`已添加房间号 ${roomId}`);
         GM_setValue('roomIds', roomIds);
+        await DMbye();
     } else {
         const index = roomIds.indexOf(roomId);
         if (index !== -1) {
             roomIds.splice(index, 1);
             console.log(`已移除房间号 ${roomId}`);
             GM_setValue('roomIds', roomIds);
+            await DMback();
         } else {
             console.log(`输入不合法`);
         }
     }
+
 }
 
 async function showSettings() {
@@ -70,7 +77,7 @@ async function showSettings() {
         message += `${i + 1}. ${idList[i]}\n`;
     }
 
-    const roomIdInput = prompt(`${message}\n请输入要移除（恢复）弹幕的房间号：（点击确定后请刷新网页以使其生效）`);
+    const roomIdInput = prompt(`${message}\n请输入要移除（恢复）弹幕的房间号房间号：`);
     if (roomIdInput) {
         updateRoomId(roomIdInput);
     }
@@ -89,6 +96,28 @@ async function addStyle(css) {
     console.log('css已插入');
 }
 
+async function DMbye(){
+    const css =`
+        .Barrage-main,
+        .comment-37342a {
+          visibility: hidden !important;
+        }
+        `;
+    console.log('已重组css' + css);
+    addStyle(css);
+}
+
+async function DMback(){
+    const css =`
+        .Barrage-main,
+        .comment-37342a {
+          visibility: unset !important;
+        }
+        `;
+    console.log('已重组css' + css);
+    addStyle(css);
+}
+
 (async function () {
     const blurcss = await blur();
     const danmucss = await danmu();
@@ -96,6 +125,3 @@ async function addStyle(css) {
     console.log('样式表已合并' + css);
     addStyle(css);
 })();
-
-//已知问题，无
-//更新计划，添加设置反馈
