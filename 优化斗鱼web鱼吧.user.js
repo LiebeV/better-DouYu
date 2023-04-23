b1bca7b2055ecc7fa68b5000765beb5a7a769615
LiebeV <68876097+LiebeV@users.noreply.github.com>
2023-04-23 22:38:35 +0200

    Update 优化斗鱼web鱼吧.user.js

--- 优化斗鱼web鱼吧.user.js
+++ 优化斗鱼web鱼吧.user.js
@@ -2,7 +2,7 @@
 // @name         优化斗鱼web鱼吧
 // @namespace    https://www.liebev.site
 // @version      0.1
-// @description  douyu，优化斗鱼web鱼吧，自动展开所有overflow内容，不给小黑子贡献阅读量
+// @description  douyu，优化斗鱼web鱼吧，自动展开所有overflow内容
 // @author       LiebeV
 // @license      MIT: Copyright (c) 2023 LiebeV
 // @match        https://yuba.douyu.com/group/*
@@ -14,7 +14,7 @@
 
 async function yuba() {
     console.log("已经创建yuba样式表");
-    return `.index-listDesc-02cUw,.index-listTitle-+WMpi{height:auto!important;white-space:normal!important;}`;
+    return `.index-listDesc-02cUw,.index-listTitle-\\+WMpi{height:auto!important;white-space:normal!important;}`;
 }
 
 async function addStyle(css) {
